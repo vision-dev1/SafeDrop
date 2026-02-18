0c4469c1939e2165b3a129f87d71aa53953deb01
Visionnn !!! <visionnn097@gmail.com>
2026-02-18 20:46:03 +0545

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,8 +1,1 @@
-"""
-SafeDrop - Secure CLI File Sharing Tool
-Developed by Vision KC
-"""
-
-__version__ = "1.0.0"
-__author__ = "Vision KC"
-__description__ = "A secure command-line file sharing tool"
+# SafeDrop Tests Package
